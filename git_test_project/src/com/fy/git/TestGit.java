d9adc6f3b142e2e61d5144c7ad2603e0db69eb83
fy <shifengyu317@gmail.com>
2020-03-24 20:41:16 +0800

    多加了两行输出

---
+++ git_test_project/src/com/fy/git/TestGit.java
@@ -0,0 +1,15 @@
+package com.fy.git;
+
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+    }
+
+}
